f4f1ef7f5e4dd5bfc0773e1d2ab4814e24b90339
camdenorrb <5360339+camdenorrb@users.noreply.github.com>
2021-09-27 06:07:44 -0500

    Make ASMOW2Cleaner.kt work in a more similar pattern

--- src/main/kotlin/dev/twelveoclock/apicreator/cleaner/ASMOW2Cleaner.kt
+++ src/main/kotlin/dev/twelveoclock/apicreator/cleaner/ASMOW2Cleaner.kt
@@ -25,14 +25,17 @@ object ASMOW2Cleaner : Cleaner {
 
 	class APIClassVisitor(api: Int, classWriter: ClassWriter) : ClassVisitor(api, classWriter) {
 
-		override fun visitAnnotation(descriptor: String?, visible: Boolean): AnnotationVisitor {
+		override fun visitAnnotation(descriptor: String?, visible: Boolean): AnnotationVisitor? {
 
+			// Remove Kotlin metadata
 			if (descriptor == "Lkotlin/Metadata;") {
-				return APIAnnotationVisitor(api, super.visitAnnotation(descriptor, visible))
+				return null
+				//return APIAnnotationVisitor(api, super.visitAnnotation(descriptor, visible))
 			}
 
 			return super.visitAnnotation(descriptor, visible)
 		}
+
 
 		override fun visitField(
 			access: Int,
@@ -45,14 +48,11 @@ object ASMOW2Cleaner : Cleaner {
 			// Write if the method is public or protected + abstract
 			// TODO: Determine whether to keep protected members based on class visibility
 
-			if (
-				access and Opcodes.ACC_PUBLIC != 0 ||
-				access and Opcodes.ACC_PROTECTED != 0
-			) {
-				return super.visitField(access, name, descriptor, signature, value)
+			if (access and Opcodes.ACC_PRIVATE != 0) {
+				return null
 			}
 
-			return null
+			return super.visitField(access, name, descriptor, signature, value)
 		}
 
 		override fun visitMethod(
@@ -65,22 +65,153 @@ object ASMOW2Cleaner : Cleaner {
 
 			// Write if the method is public or protected + abstract
 			// TODO: Determine whether to keep protected members based on class visibility
-			if (
-				access and Opcodes.ACC_PUBLIC != 0 ||
-				access and Opcodes.ACC_PROTECTED != 0
-			) {
-				super.visitMethod(access, name, descriptor, signature, exceptions)
+			if (access and Opcodes.ACC_PRIVATE != 0) {
+				return null
 			}
 
-			return null
+			return APIMethodVisitor(api, super.visitMethod(access, name, descriptor, signature, exceptions))
 		}
 
 	}
 
+	class APIMethodVisitor(api: Int, visitor: MethodVisitor) : MethodVisitor(api, visitor) {
+
+		override fun visitParameter(name: String?, access: Int) {}
+
+		override fun visitAnnotableParameterCount(parameterCount: Int, visible: Boolean) {}
+
+		override fun visitAttribute(attribute: Attribute?) {}
+
+		override fun visitCode() {}
+
+		override fun visitFrame(
+			type: Int,
+			numLocal: Int,
+			local: Array<out Any>?,
+			numStack: Int,
+			stack: Array<out Any>?,
+		) {}
+
+		override fun visitInsn(opcode: Int) {}
+
+		override fun visitIntInsn(opcode: Int, operand: Int) {}
+
+		override fun visitVarInsn(opcode: Int, `var`: Int) {}
+
+		override fun visitTypeInsn(opcode: Int, type: String?) {}
+
+		override fun visitFieldInsn(opcode: Int, owner: String?, name: String?, descriptor: String?) {}
+
+		override fun visitMethodInsn(opcode: Int, owner: String?, name: String?, descriptor: String?) {}
+
+		override fun visitMethodInsn(
+			opcode: Int,
+			owner: String?,
+			name: String?,
+			descriptor: String?,
+			isInterface: Boolean,
+		) {}
+
+		override fun visitInvokeDynamicInsn(
+			name: String?,
+			descriptor: String?,
+			bootstrapMethodHandle: Handle?,
+			vararg bootstrapMethodArguments: Any?,
+		) {}
+
+		override fun visitJumpInsn(opcode: Int, label: Label?) {}
+
+		override fun visitLabel(label: Label?) {}
+
+		override fun visitLdcInsn(value: Any?) {}
+
+		override fun visitIincInsn(`var`: Int, increment: Int) {}
+
+		override fun visitTableSwitchInsn(min: Int, max: Int, dflt: Label?, vararg labels: Label?) {}
+
+		override fun visitLookupSwitchInsn(dflt: Label?, keys: IntArray?, labels: Array<out Label>?) {}
+
+		override fun visitMultiANewArrayInsn(descriptor: String?, numDimensions: Int) {}
+
+		override fun visitInsnAnnotation(
+			typeRef: Int,
+			typePath: TypePath?,
+			descriptor: String?,
+			visible: Boolean,
+		): AnnotationVisitor? {
+			return null
+		}
+
+		override fun visitTryCatchBlock(start: Label?, end: Label?, handler: Label?, type: String?) {}
+
+		override fun visitLocalVariable(
+			name: String?,
+			descriptor: String?,
+			signature: String?,
+			start: Label?,
+			end: Label?,
+			index: Int,
+		) {}
+
+		override fun visitLineNumber(line: Int, start: Label?) {}
+
+		override fun visitMaxs(maxStack: Int, maxLocals: Int) {}
+
+		override fun visitEnd() {}
+
+		override fun visitAnnotationDefault(): AnnotationVisitor? {
+			return null
+		}
+
+		override fun visitAnnotation(descriptor: String?, visible: Boolean): AnnotationVisitor? {
+			return null
+		}
+
+		override fun visitTypeAnnotation(
+			typeRef: Int,
+			typePath: TypePath?,
+			descriptor: String?,
+			visible: Boolean,
+		): AnnotationVisitor? {
+			return null
+		}
+
+		override fun visitParameterAnnotation(
+			parameter: Int,
+			descriptor: String?,
+			visible: Boolean,
+		): AnnotationVisitor? {
+			return null
+		}
+
+		override fun visitTryCatchAnnotation(
+			typeRef: Int,
+			typePath: TypePath?,
+			descriptor: String?,
+			visible: Boolean,
+		): AnnotationVisitor? {
+			return null
+		}
+
+		override fun visitLocalVariableAnnotation(
+			typeRef: Int,
+			typePath: TypePath?,
+			start: Array<out Label>?,
+			end: Array<out Label>?,
+			index: IntArray?,
+			descriptor: String?,
+			visible: Boolean,
+		): AnnotationVisitor? {
+			return null
+		}
+	}
+
+	/*
 	class APIAnnotationVisitor(api: Int, visitor: AnnotationVisitor) : AnnotationVisitor(api, visitor) {
 
-		override fun visitArray(name: String): AnnotationVisitor {
-			return APIAnnotationArrayVisitor(api, name, super.visitArray(name))
+		override fun visitArray(name: String): AnnotationVisitor? {
+			return null
+			//return APIAnnotationArrayVisitor(api, name, super.visitArray(name))
 		}
 
 	}
@@ -95,4 +226,5 @@ object ASMOW2Cleaner : Cleaner {
 		}
 
 	}
+	*/
 }
