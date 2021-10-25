ad52e76bbcf4ca5567371e2727fbbbf882485a4c
camdenorrb <5360339+camdenorrb@users.noreply.github.com>
2021-10-25 02:25:12 -0500

    Update kotlin compile dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 
     // Testing
     testImplementation(kotlin("test-junit"))
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.4")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.5")
 }
 
 application {
